0fb1684e86940b8c925819777efff4d10d604883
evellykaysa <evellykaysa05@gmail.com>
2026-03-09 15:57:07 -0300

    Add workout functions for treino A, B, and C

---
+++ app.js
@@ -0,0 +1,17 @@
+function treinoA(){
+
+document.getElementById("resultado").innerHTML="Treino A: Peito e Tríceps"
+
+}
+
+function treinoB(){
+
+document.getElementById("resultado").innerHTML="Treino B: Costas e Bíceps"
+
+}
+
+function treinoC(){
+
+document.getElementById("resultado").innerHTML="Treino C: Pernas"
+
+}
